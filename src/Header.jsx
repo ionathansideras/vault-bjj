7ff91d95b66f29ecd5113249301c66efebd0a25a
alecnissen <alecnissen@yahoo.com>
2024-03-04 13:42:53 -0500

    added header content with logo, btns and links, now will begin main section

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,36 +1,67 @@
 import React from "react";
-
-
 import vaultBjjLogo from "../assets/logo.png"
+import instaLogo from "../assets/ig-image-transparent.png"
+import fbLogo from "../assets/fb-image-transparent.png"
 
 export default function Header() {
   return (
-    
     <div className="header-content-wrapper">
-        
-    <div className="logo-title-links-toprow-container"> 
-    
-    <h1>The Vault Jiu Jitsu</h1>
-    <h2>Delco's Premier Jiu Jitsu Academy</h2>
-    <h4>Classes for all ages and skill levels!</h4>
-        <div className="logo-container"> 
-            <img src={vaultBjjLogo}></img>
-        </div>
 
+    
+      <img src={vaultBjjLogo} className='header-vault-bjj-logo'></img>
+  
         <div className="header-btn-container">
         <button>Home</button>
         <button>About</button>
         <button>Schedule</button>
+        <button>Instructors</button>
         <button>Programs</button>
         <button>Contact/FAQ</button>
         </div>
-    
-    </div>
 
-
+      <div className="header-insta-fb-links-container">
+      <img src={instaLogo}></img>
+      <img src={fbLogo}></img>
+      </div>
 
     </div>
   )
 }
+
+
+
+
+
+
+
+// export default function Header() {
+//   return (
+    
+//     <div className="header-content-wrapper">
+        
+//     <div className="logo-title-links-toprow-container"> 
+    
+//     <h1>The Vault Jiu Jitsu</h1>
+//     <h2>Delco's Premier Jiu Jitsu Academy</h2>
+//     <h4>Classes for all ages and skill levels!</h4>
+//         <div className="logo-container"> 
+//             <img src={vaultBjjLogo}></img>
+//         </div>
+
+//         <div className="header-btn-container">
+//         <button>Home</button>
+//         <button>About</button>
+//         <button>Schedule</button>
+//         <button>Programs</button>
+//         <button>Contact/FAQ</button>
+//         </div>
+    
+//     </div>
+
+
+
+//     </div>
+//   )
+// }
 
 
--- src/Notes.jsx
+++ src/Notes.jsx
@@ -2,4 +2,9 @@
 
 // plan overall structure of the page, 
 
-// 
+// *** 3/4 
+
+
+// implement new header and welcome text section, 
+
+// buttons should be low in the screen
