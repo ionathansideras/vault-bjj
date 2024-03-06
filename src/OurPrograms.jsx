010479c6ea0989e8fb83e6bcf1c97131ed039915
alecnissen <alecnissen@yahoo.com>
2024-03-06 12:33:56 -0500

    added content and pictures for each of the program cards, they all are the same width

--- src/OurPrograms.jsx
+++ src/OurPrograms.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import nogiBjjPhoto from "../assets/g-liberty.jpg"
-import giPhoto from "../assets/hero-bg.jpg"
+import nogiBjjPhoto from "../assets/g-liberty.jpg";
+import giPhoto from "../assets/hero-bg.jpg";
+import kidsBjjPhoto from "../assets/pexels-cottonbro-studio-7988827.jpg";
+import womensBjjClassPhoto from "../assets/womens-bjj-classes-vault.jpg"
 
 export default function OurPrograms() {
   return (
@@ -19,13 +21,13 @@ export default function OurPrograms() {
         <Card className='card-container'>
 
       <div className='card-img1-container'>
-      <Card.Img variant="top" src={nogiBjjPhoto} />
+      <Card.Img variant="top" src={nogiBjjPhoto} className='program-photo' />
       </div>
 
       <Card.Body className='card-body'>
         <Card.Title>Adult Nogi</Card.Title>
         <Card.Text>
-        NoGi Jiu Jitsu simply means no “Gi” or no traditional kimono uniform. Unlike in Gi, you won’t be taught to grab the uniform at any time to execute moves. Instead, you and your other classmates will wear rash guards and board shorts (think basic surfer attire) and will employ similar classic Brazilian Jiu Jitsu moves in order to escape, block, defend, grab and pin your opponent.
+        NoGi Jiu Jitsu simply means no “Gi” or no traditional kimono uniform. NoGi uniforms consist of a rashguard and board shorts, very similar to traditional surf attire. In NoGi you will still use traditional Jiu Jitsu moves to sweep, submit, and pin your opponent.
         </Card.Text>
         <Button variant="primary">Learn More</Button>
       </Card.Body>
@@ -33,34 +35,34 @@ export default function OurPrograms() {
 
     <Card className='card-container'>
     <div className='card-img2-container'>
-      <Card.Img variant="top" src={giPhoto} />
+      <Card.Img variant="top" src={giPhoto} className='program-photo' />
       </div>
       <Card.Body className='card-body'>
         <Card.Title>Adult Gi</Card.Title>
         <Card.Text>
-        In Gi classes you will wear a traditional “Gi” or Jiu Jitsu kimono. You’ll use your skills to overpower opponents and even use their Gi to grab hold and gain the advantage over them. From learning how to escape a hold to how to pin your opponent, this class will give you all the tools you need to grow your Jiu Jitsu skills.
+        In Gi classes you will wear a traditional “Gi” or Jiu Jitsu kimono. There are subtle differences in the gripping and movement compared to NoGi Jiu-Jitsu however this is an excellent class to attend as it will make you a more complete Jiu-Jitsu practioner!
         </Card.Text>
         <Button variant="primary">Learn More</Button>
       </Card.Body>
     </Card>
 
     <Card className='card-container'>
-      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Img variant="top" src={kidsBjjPhoto} className='program-photo' />
       <Card.Body className='card-body'>
         <Card.Title>Kids Classes</Card.Title>
         <Card.Text>
-        Our kids classes are broken into two separate age groups: ages 5-8 and ages 9-12. Both groups start out slowly and safely and our instructors, trained in teaching youth, will walk them through the fundamentals of Brazilian Jiu Jitsu. It’s a great way to naturally build self-esteem, self-respect, patience and problem-solving skills in kids — in a very fun way. We make our classes safe, fun and approachable so students will enjoy being in class every week as they learn and progress.
+        Our kids classes are broken into two separate age groups: ages 5-8 and ages 9-12. We will safely walk them through the fundamentals of Jiu-Jitsu. It’s a great way to naturally build self-esteem, self-respect, patience and problem-solving skills in kids in a fun way. 
         </Card.Text>
         <Button variant="primary">Learn More</Button>
       </Card.Body>
     </Card>
 
     <Card className='card-container'>
-      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Img variant="top" src={womensBjjClassPhoto} className='program-photo card-4-img' />
       <Card.Body className='card-body'>
         <Card.Title>Women's Introductory Lessons</Card.Title>
         <Card.Text>
-        Brazilian Jiu Jitsu is most certainly not just for the guys. Women can benefit from the many perks of the practice, including weight loss, stress relief, self defense skills and body confidence. Women aren’t often encouraged to grapple or be aggressive in our everyday life. Here in our classes, we shed those gender roles and give you the techniques to dominate your opponent AND your life. Come give us a try!
+        Women can benefit from the many perks of the practice, including weight loss, stress relief, self defense skills and body confidence. In this class we will slowly and effectively walk you through the core techniques of Jiu-Jitsu and having lots of fun while doing it!
         </Card.Text>
         <Button variant="primary">Learn More</Button>
       </Card.Body>
